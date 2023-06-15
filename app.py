db25be1889afeff4c1ebdbb1b4c5c2ecaff6f691
victor nta <victornta32@gmail.com>
2023-06-15 19:50:40 +0100

    commit

--- app.py
+++ app.py
@@ -1,47 +1,42 @@
-from flask import Flask,request,render_template
-import numpy as np
-import pandas as pd
-
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import uvicorn 
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-application=Flask(__name__)
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-app=application
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-## Route for a home page
+@app.get("/home")
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
-@app.route('/')
-def index():
-    return render_template('index.html') 
+@app.post("/predictdata")
+async def predict_datapoint(request: Request):
+    form_data = await request.form()
+    data = CustomData(
+        gender=form_data.get('gender'),
+        race_ethnicity=form_data.get('ethnicity'),
+        parental_level_of_education=form_data.get('parental_level_of_education'),
+        lunch=form_data.get('lunch'),
+        test_preparation_course=form_data.get('test_preparation_course'),
+        reading_score=float(form_data.get('writing_score')),
+        writing_score=float(form_data.get('reading_score'))
+    )
+    pred_df = data.get_data_as_data_frame()
+    print(pred_df)
+    print("Before Prediction")
+    predict_pipeline = PredictPipeline()
+    print("Mid Prediction")
+    results = predict_pipeline.predict(pred_df)
+    print("after Prediction")
+    return templates.TemplateResponse("home.html", {"request": request, "results": results[0]})
 
-@app.route('/predictdata',methods=['GET','POST'])
-def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
-    else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
-
-        predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
-    
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0")        
-
-
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0")
